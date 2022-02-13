35e0e5d6d18b7ab52b671d3540ddf0b71f5bc4e5
dinhhailinh <58936308+dinhhailinh@users.noreply.github.com>
2022-02-13 14:31:44 +0700

    commit

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -13,7 +13,7 @@ const protect = async (req, res, next) => {
 
       const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
-      req.user = await User.findById(decoded.id).select('-password')
+      req.user = await User.findById(decoded.id)
 
       next()
     } catch (error) {
