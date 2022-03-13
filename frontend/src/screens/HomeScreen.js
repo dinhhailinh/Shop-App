de77bfd150cf676eb34babe943b600f2841903f0
dinhhailinh <58936308+dinhhailinh@users.noreply.github.com>
2022-03-13 16:33:58 +0700

    Update HomeScreen.js

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -19,7 +19,7 @@ const HomeScreen = ({ match }) => {
 
   const productList = useSelector((state) => state.productList)
   const { loading, error, products, page, pages } = productList
-
+  
   useEffect(() => {
     dispatch(listProducts(keyword, pageNumber))
   }, [dispatch, keyword, pageNumber])
